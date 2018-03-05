c4a21984befc8eabfb63723537acfb4402db1d65
zyw327 <327523057@qq.com>
2018-03-05 22:41:22 +0800

    use extarct-text-webpack-plugin

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 ## vue1.0与webpack组合搭建项目
-### 执行webpack
+### 执行webpack编译生成静态资源
+## 运行服务
+```shell
+    npm run server
+```
+run at http://localhost:8000/
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     output:{
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        // publicPath: 'public'
+        publicPath: 'public'
     },
     module: {
         rules: [
